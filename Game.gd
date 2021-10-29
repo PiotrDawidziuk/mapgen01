0126d59b0fdb14eed0f415123516e740cdbb84ed
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2021-10-29 19:17:15 +0200

    generates full wall map

--- Game.gd
+++ Game.gd
@@ -16,7 +16,7 @@ const LEVEL_ROOM_COUNTS = [5,7,9,12,15]
 const MIN_ROOM_DIMENSION = 5
 const MAX_ROOM_DIMENSION = 8
 
-enum Tile {Door, Floor, Player, Wall}
+enum Tile {Floor, Wall, Player, Door}
 
 #Current level
 var level_num = 0
@@ -29,5 +29,32 @@ var level_size
 onready var tile_map = $TileMap
 onready var player = $Player
 
+# Game state
+
+var player_tile
+var score = 0
+
 func _ready():
 	OS.set_window_size(Vector2(1280,720))
+	randomize()
+	build_level()
+
+func build_level():
+	# start with blank map
+	
+	rooms.clear()
+	map.clear()
+	tile_map.clear()
+	
+	level_size = LEVEL_SIZES[level_num]
+	for x in range (level_size.x):
+		map.append([])
+		for y in range (level_size.y):
+			map[x].append(Tile.Wall)
+			tile_map.set_cell(x,y,Tile.Wall)
+			
+
+
+
+
+
