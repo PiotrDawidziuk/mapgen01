f3c89cf3e430d95fbf70932b5cf4a3b88bdc19d6
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2021-11-01 22:10:33 +0100

    player can move around the map

--- Game.gd
+++ Game.gd
@@ -3,6 +3,7 @@ extends Node2D
 #objects created from other classes
 var level_builder = LevelBuilder.new()
 var consts = GameConsts.new()
+var tile_setter = TileSetter.new()
 
 #Current level
 var level_num = 0
@@ -30,7 +31,35 @@ func _ready():
 	
 	update_visuals()
 	
+func _input(event):
+	if !event.is_pressed():
+		return
+	
+	if event.is_action("Left"):
+		try_move(-1,0)
+	if event.is_action("Right"):
+		try_move(1,0)
+	if event.is_action("Up"):
+		try_move(0,-1)
+	if event.is_action("Down"):
+		try_move(0,1)
+		
+func try_move(dx,dy):
+	var x = player_tile.x + dx
+	var y = player_tile.y + dy
+	var tile_type = consts.Tile.Stone
+	#if x >= 0 && x < level_size.x && y >= 0 && y <level_size.y:
+	tile_type = map [x][y]
+		
+	match tile_type:
+		consts.Tile.Floor:
+				player_tile = Vector2(x,y)
+			
+		consts.Tile.Door: 
+			tile_setter.set_tile(x,y,consts.Tile.Floor, map, tile_map)	
+	update_visuals()
 
 func update_visuals():
 
 	player.position = player_tile * consts.TILE_SIZE
+	yield(get_tree().create_timer(0.000001), "timeout")
